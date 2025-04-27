83ea2c1b638f147199279a4504d2ab7b15bd6215
mina218 <amina.yahia@eniso.u-sousse.tn>
2025-04-27 15:05:24 +0200

    [start]

--- lib/aws_helper_engine/aws_config.rb
+++ lib/aws_helper_engine/aws_config.rb
@@ -1,25 +1,39 @@
 # frozen_string_literal: true
-# this module is for connecting to aws account
+
+# This module is for connecting to an AWS account
 require "aws-sdk-core"
+
 module AwsHelperEngine
   module S3
-  class AwsConfig
-  class << self
-    def instance
-      @instance ||= new
-    end
-    attr_reader :access_key_id, :secret_access_key, :http, :options
+    class AwsConfig
+      attr_reader :access_key_id, :secret_access_key, :http, :options
 
-    def initialize(super_class = nil)
-      super
-      instance.configure(access_key_id, secret_access_key, http, options)
+      def initialize(access_key_id: nil, secret_access_key: nil, http: nil, options: {})
+        @access_key_id = access_key_id
+        @secret_access_key = secret_access_key
+        @http = http
+        @options = options
+      end
+
+      class << self
+        def instance
+          @instance ||= new
+        end
+      end
+
+      # This method will configure the credentials manually
+      def configure(access_key_id:, secret_access_key:, http: nil, options: {})
+        @access_key_id = access_key_id
+        @secret_access_key = secret_access_key
+        @http = http
+        @options = options
+      end
+
+      # This method can authenticate using IAM roles (example, not complete)
+      def authenticate_with_iam_role
+        # Here you can load credentials from environment or instance profile
+        Aws::STS::Client.new
+      end
     end
   end
-
-
-end
-  #this methode to authentificate using IAMroles
-  #
-
-end
 end

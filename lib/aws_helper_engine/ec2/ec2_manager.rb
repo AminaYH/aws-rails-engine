99613e4d0829d99164a07fcf53525521c786e667
mina218 <amina.yahia@eniso.u-sousse.tn>
2025-05-10 18:35:50 +0200

    [ec2-instances]

---
+++ lib/aws_helper_engine/ec2/ec2_manager.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+require 'aws-sdk-ec2'
+require 'logger'
+module AwsHelperEngine
+  module Ec2
+
+class EC2Manager
+  def initialize(client)
+    @client = client
+    @logger = Logger.new($stdout)
+  end
+
+  def list_instances
+  @logger.info('Listing instances')
+  instances = fetch_instances
+  if instances.empty?
+    @logger.info('You have no instances')
+  else
+    print_instances(instances)
+  end
+end
+  def fetch_instances
+    paginator = @client.describe_instances
+    instances = []
+    paginator.each_page do |page|
+      page.reservations.each do |reservation|
+        reservation.instances.each do |instance|
+          instances << instance
+        end
+      end
+    end
+    instances
+  end
+  def print_instances(instances)
+    instances.each do |instance|
+      @logger.info("Instance ID: #{instance.instance_id}")
+      @logger.info("Instance Type: #{instance.instance_type}")
+      @logger.info("Public IP: #{instance.public_ip_address}")
+      @logger.info("Public DNS Name: #{instance.public_dns_name}")
+      @logger.info("\n")
+    end
+  end
+
+end
+end
+end
---
+++ lib/aws_helper_engine/ec2/key_pair.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+# frozen_string_literal: true
+require 'aws-sdk-ec2'
+require 'logger'
+module AwsHelperEngine
+  module Ec2
+class KeyPair
+end
+end
+end

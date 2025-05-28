25d1af54a965867c6dabd4601b641982562c02f8
mina218 <amina.yahia@eniso.u-sousse.tn>
2025-05-29 01:02:00 +0200

    [sns] start

---
+++ lib/aws_helper_engine/sns/sns_client.rb
@@ -0,0 +1,20 @@
+require "aws-sdk-sns"
+
+module AwsHelperEngine 
+module Sns
+ class SnsClient
+  def initialize
+  end
+  def create_topic
+  end
+  def susbscribe
+  end
+  def unsubscribe
+  end
+  def  delete_topic
+  end
+  def list_topics
+  end
+end
+end
+end

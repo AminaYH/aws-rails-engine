618f5a957c4202b16a8ac4347d837e197c089d93
mina218 <amina.yahia@eniso.u-sousse.tn>
2025-05-29 13:04:58 +0200

    [sns] client

---
+++ lib/aws_helper_engine/sns/client.rb
@@ -0,0 +1,17 @@
+require "aws-sns-sdk"
+module AwsHelperEngine
+module sns
+class client
+  attr_reader: client
+  def initialize(region:"us-east-1", access_key_id: nil, secret_access_key: nil, session_token: nil)
+    @client=AWS::SNS::Client.new(
+      region: region,
+      access_key_id:access_key_id,
+      secret_access_key: secret_access_key,
+      session_token: session_token
+    )
+  end
+end
+
+end
+end
--- lib/aws_helper_engine/sns/sns_client.rb
+++ lib/aws_helper_engine/sns/sns_client.rb
@@ -3,18 +3,47 @@ require "aws-sdk-sns"
 module AwsHelperEngine 
 module Sns
  class SnsClient
-  def initialize
+  def initialize(client,topic_name,tags: [])
+    @client=client
+    @topic_name=topic_name
+    @tags = tags
+
   end
   def create_topic
+        resp = @client.create_topic(
+          name: @topic_name,
+          tags: @tags
+        )
+        resp.topic_arn
   end
-  def susbscribe
+   def subscribe(topic_arn:, protocol:, endpoint:)
+        @client.subscribe(
+          topic_arn: topic_arn,
+          protocol: protocol,     
+          endpoint: endpoint      
+        )
+      end
+      #only if you are owner of topic 
+  def unsubscribe(subscription_arn:)
+  raise ArgumentError, "subscription_arn is required" if subscription_arn.nil? || subscription_arn.empty?
+
+  begin
+    @client.unsubscribe(subscription_arn: subscription_arn)
+    puts "Successfully unsubscribed from #{subscription_arn}"
+  rescue Aws::SNS::Errors::AuthorizationError => e
+    puts "Not allowed to unsubscribe. Probably not the owner or insufficient permissions: #{e.message}"
+  rescue Aws::SNS::Errors::ServiceError => e
+    puts "Failed to unsubscribe due to AWS error: #{e.message}"
   end
-  def unsubscribe
-  end
-  def  delete_topic
-  end
+end
+
+   def delete_topic(topic_arn:)
+        @client.delete_topic(topic_arn: topic_arn)
+      end
   def list_topics
-  end
+        @client.list_topics.topics.map(&:topic_arn)
+      end
+
 end
 end
 end
